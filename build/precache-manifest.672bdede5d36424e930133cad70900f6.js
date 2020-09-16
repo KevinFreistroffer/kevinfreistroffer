70e17c22c7bdc9e7465210755b244c758c0e1ffb
Kevin <kevin.freistroffer@gmail.com>
2020-09-16 12:45:01 -0700

    build

---
+++ build/precache-manifest.672bdede5d36424e930133cad70900f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ddfee0f07e0577e37541e3246ae43e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f38d8b98ebcf0378d1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "616f9ab09e1518b32122",
+    "url": "/static/js/2.f0daf8c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0daf8c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f38d8b98ebcf0378d1",
+    "url": "/static/js/main.26c09081.chunk.js"
+  },
+  {
+    "revision": "25ab48af68eef122e093",
+    "url": "/static/js/runtime-main.343b3593.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
