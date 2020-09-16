5f3996724ce24196990078ac58fc719a21ccb750
Kevin <kevin.freistroffer@gmail.com>
2020-09-16 14:22:00 -0700

    /Header and Header.js added

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+// @flow",
+import React, { useState, useEffect } from "react";
+
+type Props = {};
+
+function Header(props: Props) {
+  return <div>Header functional component</div>;
+}
+
+export default Header;
---
+++ src/layout/Page.js
@@ -0,0 +1,18 @@
+// @flow",
+import React, { Component } from "react";
+
+type Props = {};
+type State = {};
+
+class Page extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Page class component</div>;
+  }
+}
+
+export default Page;
