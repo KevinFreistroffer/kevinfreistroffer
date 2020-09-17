4bc39896b86a6b43059f9891d4bf1a114d65209a
Kevin <kevin.freistroffer@gmail.com>
2020-09-17 11:00:27 -0700

    meApi added to amplify server

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
-
+import Icon from "@material-ui/core/Icon";
 import { AppContext } from "../state/app_state";
 import "./header_styles.scss";
 import { Route } from "../routes";
@@ -10,27 +10,33 @@ interface Props {
   routes: Route[];
 }
 
-function Header(props: Props) {
-  let context = React.useContext(AppContext);
-  return (
-    <header id="app-header" className="flex row ">
-      <NavBar title={context.title} routes={props.routes}></NavBar>
-    </header>
-  );
-}
-
 function NavBar(props: { title: string; routes: Route[] }) {
   return (
-    <nav id="app-navbar" className="flex row align-center">
+    <nav
+      id="app-navbar"
+      className="flex row align-center justify-space-between"
+    >
       <h1>{props.title}</h1>
-      <ul>
+      <Icon>
+      <ul className="flex row">
         {props.routes.map(route => (
-          <Link key={route.id} to={route.path}>
-            {route.text}
-          </Link>
+          <li>
+            <Link key={route.id} to={route.path}>
+              {route.text}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
+  );
+}
+
+function Header(props: Props) {
+  let context = React.useContext(AppContext);
+  return (
+    <header id="app-header" className="flex row">
+      <NavBar title={context.title} routes={props.routes}></NavBar>
+    </header>
   );
 }
 
