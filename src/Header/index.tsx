a9a89289ee3ec5c5f86b7e418105f1af46d8ee4c
Kevin <kevin.freistroffer@gmail.com>
2020-09-20 22:46:13 -0700

    Installed packagee react-addons-perf. Imported Perf into Header.tsx. Deleted Button import from material-ui in Header.tsx

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
+import Perf from "react-addons-perf";
 import NavBar from "./components/NavBar";
 
-import { AppContext } from "../state/app_state";
+import { AppContext, AppState } from "../state/app_state";
 import "./styles.scss";
 import { Route } from "../routes";
+
 type Props = {
   routes: Route[];
 };
 
 const Header: React.FC<Props> = (props: Props) => {
+  useEffect(() => {}, []);
+
   let context = React.useContext(AppContext);
   return (
     <header id="app-header" className="flex row">
